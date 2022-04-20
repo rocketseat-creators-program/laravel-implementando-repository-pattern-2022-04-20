e4448f1bc051fc0d116b67829977f7b145041cb2
Patrick Maciel <patrickmaciel.info@gmail.com>
2022-04-20 17:37:05 -0300

    adicionado o tailwindcss

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Laravel 8 + Repository</title>
+</head>
+<body>
+    @yield('content')
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
